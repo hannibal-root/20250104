2fa6cff4c4f501867ca23d09b4e7a7c1018b805a
gkisvarga <gabor.kisvarga@blackbelt.hu>
2025-01-04 14:25:25 +0100

    StringManipulatorTest

---
+++ src/test/java/StringManipulatorTest.java
@@ -0,0 +1,24 @@
+import org.example.StringManipulator;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringManipulatorTest {
+
+    StringManipulator manipulator = new StringManipulator();
+
+    @Test
+    public void testConcatenate() {
+        assertEquals("HelloWorld", manipulator.concatenate("Hello", "World"), "Concatenation should work as expected");
+    }
+
+    @Test
+    public void testIsPalindromeTrue() {
+        assertTrue(manipulator.isPalindrome("madam"), "madam should be a palindrome");
+    }
+
+    @Test
+    public void testIsPalindromeFalse() {
+        assertFalse(manipulator.isPalindrome("hello"), "hello should not be a palindrome");
+    }
+}
